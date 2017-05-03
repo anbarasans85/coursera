60101ec93b1ae3dc0a933a57cb47c5a254f48a45
Anbarasan <anbarasans85@gmail.com>
2017-05-03 08:10:00 -1000

    problem 5

--- dsa_ucsd/001_algorithmic_toolbox/week_02/01_introduction_starter_files/fibonacci_huge/fibonacci_huge.py
+++ dsa_ucsd/001_algorithmic_toolbox/week_02/01_introduction_starter_files/fibonacci_huge/fibonacci_huge.py
@@ -1,19 +1,37 @@
 # Uses python3
 import sys
 
-def get_fibonacci_huge_naive(n, m):
+
+def calc_fib(n):
     if n <= 1:
         return n
-
     previous = 0
-    current  = 1
-
+    current = 1
     for _ in range(n - 1):
         previous, current = current, previous + current
+    return current
 
-    return current % m
+
+def pisano_period_length(pm):
+    length = 2
+    fibp = 1
+    previous = 1
+    current = 1
+    while 1:
+        previous, current = current, previous + current
+        fib1 = current % pm
+        if fibp == 0 and fib1 == 1:
+            return length
+        fibp = fib1
+        length += 1
+
+
+def get_fibonacci_huge(n, m):
+    reminder = n % pisano_period_length(m)
+    return calc_fib(reminder) % m
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
-    n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    inputn = sys.stdin.read()
+    ni, mi = map(int, inputn.split())
+    print(get_fibonacci_huge(ni, mi))
