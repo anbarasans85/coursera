ccb665d00096588b0339d7935fc278707b16fb73
Anbarasan <anbarasans85@gmail.com>
2017-05-03 10:14:31 -1000

    coursera

--- dsa_ucsd/001_algorithmic_toolbox/week_02/01_introduction_starter_files/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
+++ dsa_ucsd/001_algorithmic_toolbox/week_02/01_introduction_starter_files/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
@@ -1,19 +1,36 @@
 # Uses python3
 import sys
 
-def fibonacci_sum_naive(n):
+def fibonacci(n):
     if n <= 1:
         return n
-
     previous = 0
-    current  = 1
-    sum      = 1
-
+    current = 1
     for _ in range(n - 1):
         previous, current = current, previous + current
-        sum += current
+    return current
 
-    return sum % 10
+
+def pisano_period_length(pm):
+    length = 2
+    fibp = 1
+    previous = 1
+    current = 1
+    while 1:
+        previous, current = current, previous + current
+        fib1 = current % pm
+        if fibp == 0 and fib1 == 1:
+            return length
+        fibp = fib1
+        length += 1
+
+
+def fibonacci_sum_naive(n):
+    pperiod = pisano_period_length(10)
+    reminder = n % pperiod
+    print(fibonacci(reminder + 2) - 1)
+    return (fibonacci(reminder + 2) - 1) % 10
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
