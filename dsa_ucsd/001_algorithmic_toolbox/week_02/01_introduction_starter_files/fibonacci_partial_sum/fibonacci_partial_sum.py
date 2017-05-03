bb47a7d565d64fe87b2f11ea2923b58f85763c82
Anbarasan <anbarasans85@gmail.com>
2017-05-03 10:14:04 -1000

    coursera

--- dsa_ucsd/001_algorithmic_toolbox/week_02/01_introduction_starter_files/fibonacci_partial_sum/fibonacci_partial_sum.py
+++ dsa_ucsd/001_algorithmic_toolbox/week_02/01_introduction_starter_files/fibonacci_partial_sum/fibonacci_partial_sum.py
@@ -1,26 +1,45 @@
 # Uses python3
 import sys
 
-def fibonacci_partial_sum_naive(from_, to):
-    if to <= 1:
-        return to
 
+def fibonacci(n):
+    if n <= 1:
+        return n
     previous = 0
-    current  = 1
-
-    for _ in range(from_ - 1):
+    current = 1
+    for _ in range(n - 1):
         previous, current = current, previous + current
+    return current
 
-    sum = current
 
-    for _ in range(to - from_):
+def pisano_period_length(pm):
+    length = 2
+    fibp = 1
+    previous = 1
+    current = 1
+    while 1:
         previous, current = current, previous + current
-        sum += current
+        fib1 = current % pm
+        if fibp == 0 and fib1 == 1:
+            return length
+        fibp = fib1
+        length += 1
 
-    return sum % 10
+
+def fibonacci_sum_naive(n):
+    pperiod = pisano_period_length(10)
+    reminder = n % pperiod
+    return (fibonacci(reminder + 2) - 1) % 10
+
+
+def fibonacci_partial_sum_naive(fromi, to):
+    pperiod = pisano_period_length(100)
+    reminder1 = to % pperiod
+    reminder2 = (fromi - 1) % pperiod
+    return ((fibonacci(reminder1 + 2) - 1) - (fibonacci(reminder2 + 2) - 1)) % 10
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
+    input = sys.stdin.read()
     from_, to = map(int, input.split())
     print(fibonacci_partial_sum_naive(from_, to))
